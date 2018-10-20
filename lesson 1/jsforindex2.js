bfbf3afbf64ec34470609100693b39958ea51386
IchMag <41261762+IchMag@users.noreply.github.com>
2018-10-20 21:10:15 +0200

    Hausaufgabe3

--- lesson 1/jsforindex2.js
+++ lesson 1/jsforindex2.js
@@ -1,30 +1,72 @@
 var mainList = {
-	budget:  firmaBudget,
+	budget:  '',
 	nameofshop: '',
 	shopGoods: ['Kartoffel' , 'karotte' , 'löffel'],
-	employers: {},
+	employers: ['' , ''],
 	open: true
 }
 
+var worker = {
+	ids: []
+}
+
+function ourWorkers () {
+	for (let x = 0 ; x <= 1 ; x++){
+		var v = 1;
+		var workerName = prompt('Wie heißt Worker');
+		if (isNaN(workerName) && workerName != ''){
+			mainList.employers[x] = workerName;
+		}
+
+
+		var workerId = prompt('What is worker Id?');
+		if (workerId != 'string' && workerId != ''){
+			worker.ids[x] = workerId;
+		}
+		++v
+	}
+}
+
+
 function shopGoods (count) {
+
 	for (let i = 0 ; i <= count ; i++){
-		var save = prompt('Was möchtest du in deinem Shop verkaufen?');
-		if (mainList.shopGoods[i] == "") {
-			mainList.shopGoods[i] = save;
+		var input = prompt('what do you want to sell?');
+        if(isNaN(input) && (typeof(input)) === 'string' && input != ''){
+        	mainList.shopGoods[i] = input;
+		}
+	}	
+}
+
+var acept = 0;
+
+function budgetGet () {
+	if (acept == 0) {
+		var mybudget = prompt("Wie hoch ist Ihr Budget?")
+		if (mybudget != isNaN(mybudget) && mybudget != ''){
+			mainList.budget = mybudget;
+			++acept;
 		}
 	}
 }
 
-shopGoods(3);
+
+shopGoods(2);
+budgetGet();
+ourWorkers();
 
 
-mainList.budget = +prompt('Wie groß ist dein Budget?');
+
+
+// mainList.budget = +prompt('Wie groß ist dein Budget?');
 mainList.nameofshop = prompt('Wie heißt dein Shop?');
 
 var firmaBudget = mainList.budget;
-var firmaName = mainList.nameofshop;
+// var firmaName = mainList.nameofshop;
 
 console.log(mainList);
+console.log(worker);
 console.log('The Budget is: ' + firmaBudget);
-console.log('The Name is: ' + firmaName);
-
+console.log('The Name is:' + mainList.nameofshop);
+console.log('Your workers are: ' + mainList.employers[0] + (' , ') + mainList.employers[1]);
+console.log('Worker1 ID = ' + worker.ids[0] + (' , ') + 'Worker2 ID = ' + worker.ids[1]);
